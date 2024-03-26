da034c2786171527f0317fb030338da0550c7c0b
Julian Gruber <julian@juliangruber.com>
2024-03-26 14:16:04 +0100

    double traffic

--- voyager.js
+++ voyager.js
@@ -59,8 +59,8 @@ export async function runSaturnBenchmarkInterval() {
     } else {
         await runSaturnBenchmark()
     }
-    console.log('Sleeping for 2s...')
-    setTimeout(runSaturnBenchmarkInterval, 1000 * 2)
+    console.log('Sleeping for 1s...')
+    setTimeout(runSaturnBenchmarkInterval, 1000)
 }
 
 async function runSaturnBenchmark() {
