776e4c79397fdc54cb7a77e0a134085b9827a564
Julian Gruber <julian@juliangruber.com>
2024-06-03 12:15:30 +0200

    fix module exiting

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-// The Voyager program has ended. Thank you all for participating! We're
-// going to retroactively reward all participants. Stay tuned!
+console.error('The Voyager program has ended. Thank you all for participating!')
+console.error('We\'re going to retroactively reward all participants.')
+console.error('Stay tuned!')
+while (true) await new Promise(resolve => setTimeout(resolve, 10_000))
 
 // import { runSaturnBenchmarkLoop } from './voyager.js'
 
